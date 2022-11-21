785ad58ae9fa726123848b93bbbd4f6a45a3290e
manthan0112 <shahmanthan0112@gmail.com>
2022-11-21 16:44:13 +0530

    Math Problems Added

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,5 +1,6 @@
 package org.example;
 
+import org.example.math.*;
 import java.util.Scanner;
 import java.util.Stack;
 import org.example.Stack.*;
@@ -7,53 +8,54 @@ import org.example.Stack.*;
 public class Main {
     public static void main(String[] args) {
 
-        Scanner sc = new Scanner(System.in);
-        int choice = 0;
-        boolean flag=true;
-        while(flag)
-        {
-            System.out.println("enter 1 for stack operations 2 for sorting 3 for  exit::");
-            choice= sc.nextInt();
-            switch (choice) {
-                case 1:
-                    System.out.println("enter choice: 1 for maxDepth \n enter 2 for removeDuplicate \n enter 3 for valid parenthesis::");
-                    int ch=sc.nextInt();
-                    switch (ch){
-                        case 1:
-                            System.out.println("enter a string:");
-                            String s=sc.next();
-
-                            new maxDepth().maxdepth(s);
-                            break;
-                        case 2:
-                            System.out.println("enter a string:");
-                            String s1=sc.next();
-                            new removeDuplicate().removeduplicates(s1);
-                            break;
-                        case 3:
-                            System.out.println("enter a string:");
-                            String s2=sc.next();
-                            new validParenthesis().isValid(s2);
-                            break;
-                        default:
-                            System.out.println("entered wrong input");
-                    }
-
-
-
-                case 2:
+        System.out.println(new CountPrimes().countPrimes(45));
+//        Scanner sc = new Scanner(System.in);
+//        int choice = 0;
+//        boolean flag=true;
+//        while(flag)
+//        {
+//            System.out.println("enter 1 for stack operations 2 for sorting 3 for  exit::");
+//            choice= sc.nextInt();
+//            switch (choice) {
+//                case 1:
+//                    System.out.println("enter choice: 1 for maxDepth \n enter 2 for removeDuplicate \n enter 3 for valid parenthesis::");
+//                    int ch=sc.nextInt();
+//                    switch (ch){
+//                        case 1:
+//                            System.out.println("enter a string:");
+//                            String s=sc.next();
 //
-
-                case 3:
-                    flag=false;
-                    System.out.println("exiting");
-                    break;
-                default:
-                    System.out.println("wrong input entered");
-            }
-
-
-
-        }
+//                            new maxDepth().maxdepth(s);
+//                            break;
+//                        case 2:
+//                            System.out.println("enter a string:");
+//                            String s1=sc.next();
+//                            new removeDuplicate().removeduplicates(s1);
+//                            break;
+//                        case 3:
+//                            System.out.println("enter a string:");
+//                            String s2=sc.next();
+//                            new validParenthesis().isValid(s2);
+//                            break;
+//                        default:
+//                            System.out.println("entered wrong input");
+//                    }
+//
+//
+//
+//                case 2:
+////
+//
+//                case 3:
+//                    flag=false;
+//                    System.out.println("exiting");
+//                    break;
+//                default:
+//                    System.out.println("wrong input entered");
+//            }
+//
+//
+//
+//        }
     }
 }
