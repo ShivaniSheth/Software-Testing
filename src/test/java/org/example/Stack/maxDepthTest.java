261233477106b30e00965ccd2141aa06d36532ba
ShivaniSheth <shethsv24@gmail.com>
2022-11-21 16:26:04 +0530

    jdk version changed

---
+++ src/test/java/org/example/Stack/maxDepthTest.java
@@ -0,0 +1,13 @@
+package org.example.Stack;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class maxDepthTest {
+
+    @Test
+    void maxdepth() {
+
+    }
+}
