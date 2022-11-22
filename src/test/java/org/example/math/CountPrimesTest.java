ef037accd99c31751851b87d56a02998553ebc5d
manthan0112 <shahmanthan0112@gmail.com>
2022-11-22 11:51:27 +0530

    few more changes

---
+++ src/test/java/org/example/math/CountPrimesTest.java
@@ -0,0 +1,12 @@
+package org.example.math;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class CountPrimesTest {
+
+    @Test
+    void countPrimes() {
+    }
+}
