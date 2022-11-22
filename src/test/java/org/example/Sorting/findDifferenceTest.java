969d142ac7ca7af6a3e8fbdb04e7d57e87b82118
Ashu-Soni <ashu07101998@gmail.com>
2022-11-22 23:02:39 +0530

    test cases added for sorting

--- src/test/java/org/example/Sorting/findDifferenceTest.java
+++ src/test/java/org/example/Sorting/findDifferenceTest.java
@@ -8,6 +8,16 @@ class findDifferenceTest {
 findDifference f=new findDifference();
     @Test
     void finddifference() {
+
+        // Test cases for edge coverage
+        assertEquals('a',f.finddifference("",""));
+        assertEquals('e',f.finddifference("abcd","abcde"));
+
+        // Test cases for prime path coverage
         assertEquals('y',f.finddifference("","y"));
+        assertEquals('a',f.finddifference("",""));
+        assertEquals('e',f.finddifference("abc","abce"));
+        assertEquals('a',f.finddifference("b","ba"));
+        assertEquals('b',f.finddifference("a","ab"));
     }
 }
--- src/test/java/org/example/Sorting/squaresofSortedArrayTest.java
+++ src/test/java/org/example/Sorting/squaresofSortedArrayTest.java
@@ -8,8 +8,39 @@ class squaresofSortedArrayTest {
 squaresofSortedArray s=new squaresofSortedArray();
     @Test
     void sortedSquares() {
-        int list1[]={-4,-1,0,3,10};
-        int list2[]={0,1,9,16,100};
+
+        // Test cases for edge coverage
+        int list1[]={};
+        int list2[]={};
+        assertArrayEquals(list2,s.sortedSquares(list1));
+
+        list1=new int[]{3};
+        list2=new int[]{9};
+        assertArrayEquals(list2,s.sortedSquares(list1));
+
+        list1=new int[]{-1,3,7};
+        list2=new int[]{1,9,49};
+        assertArrayEquals(list2,s.sortedSquares(list1));
+
+        // Test cases for prime path coverage
+        list1=new int[]{};
+        list2=new int[]{};
+        assertArrayEquals(list2,s.sortedSquares(list1));
+
+        list1=new int[]{3};
+        list2=new int[]{9};
+        assertArrayEquals(list2,s.sortedSquares(list1));
+
+        list1=new int[]{3,6};
+        list2=new int[]{9,36};
+        assertArrayEquals(list2,s.sortedSquares(list1));
+
+        list1=new int[]{2,5,7};
+        list2=new int[]{4,25,49};
+        assertArrayEquals(list2,s.sortedSquares(list1));
+
+        list1=new int[]{-4,1,2};
+        list2=new int[]{1,4,16};
         assertArrayEquals(list2,s.sortedSquares(list1));
     }
 }
