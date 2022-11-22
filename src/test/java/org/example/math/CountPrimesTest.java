07c9d2831e404cc432d546f979873dd8d83a8208
ShivaniSheth <shethsv24@gmail.com>
2022-11-22 12:14:23 +0530

    Main function modified

---
+++ src/test/java/org/example/math/CountPrimesTest.java
@@ -0,0 +1,14 @@
+package org.example.math;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class CountPrimesTest {
+CountPrimes c=new CountPrimes();
+
+    @Test
+    void countPrimes() {
+        assertEquals(4,c.countPrimes(10));
+    }
+}
